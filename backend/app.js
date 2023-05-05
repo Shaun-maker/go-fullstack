3dedaae79dbbb3b26204981ece79b55b071fc238
Axel Paillaud <a.paillaud75@gmail.com>
2023-05-05 11:28:19 +0200

    separate routes from app.js inside router, separate jobs function inside controllers

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
-const Thing = require('./models/Thing');
-
 mongoose.connect('mongodb+srv://apaillaud:TLsHOTdjfaXSHZsf@go-fullstack.79gqop2.mongodb.net/?retryWrites=true&w=majority',
   { useNewUrlParser: true,
     useUnifiedTopology: true })
@@ -20,38 +18,8 @@ app.use((req, res, next) => {
     next();
   });
 
-app.post('/api/stuff', (req, res, next) => {
-    delete req.body._id;
-    const thing = new Thing({
-        ...req.body
-    });
-    thing.save()
-    .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
-    .catch(error => res.status(400).json({ error }));
-})
+const stuffRoutes = require('./routes/stuff');
 
-app.put('/api/stuff/:id', (req, res) => {
-    Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Objet modifié !'}))
-    .catch(error => res.status(400).json({ error }));
-});
-
-app.delete('/api/stuff/:id', (req, res) => {
-    Thing.deleteOne({ _id: req.params.id })
-    .then(res.status(200).json({ message: 'Objet supprimé !'}))
-    .catch(error => res.status(400).json({ error }));
-})
-
-app.get('/api/stuff', (req, res, next) => {
-    Thing.find()
-    .then(things => res.status(200).json(things))
-    .catch(error => res.status(400).json({ error }));
-})
-
-app.get('/api/stuff/:id', (req, res) => {
-    Thing.findOne({ _id: req.params.id})
-    .then(thing => res.status(200).json(thing))
-    .catch(error => res.status(404).json({ error }));
-});
+app.use('/api/stuff', stuffRoutes);
 
 module.exports = app;
